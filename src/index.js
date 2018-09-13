e25711c3a23ca2ea38d1c559b038fe392a070533
tamisharutledge <tamisha.rutledge@gmail.com>
2018-09-12 20:53:27 -0500

    Movie Lister

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
  * es6 modules and imports
  */
 import sayHello from './hello';
+
 sayHello('World');
 
 /**
@@ -9,12 +10,35 @@ sayHello('World');
  */
 const {getMovies} = require('./api.js');
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
+
+
+
+
+$("#info").append("Loading...");
+
+$.ajax('/api/movies').done(function (data) {
+    $("#info").html("");
+
+
+    data.forEach(function (movie) {
+        $("#info").append(movie.title);
+
+
+        // alert("Everything went great! Check out the server's response in the console.");
+        console.log(data[0]);
+        console.log(data[1]);
+    });
+
 });
+
+// getMovies().then((movies) => {
+//   $("#loading").hide();
+//   console.log('Here are all the movies:');
+//   movies.forEach(({title, rating, id}) => {
+//     console.log(`id#${id} - ${title} - rating: ${rating}`);
+//   });
+// }).catch((error) => {
+//   alert('Oh no! Something went wrong.\nCheck the console for details.')
+//   console.log(error);
+// });
+
